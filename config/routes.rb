0d096edc806c6709526c173e810136d923ecf1f3
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-10-05 17:42:24 -0400

    adds users index and tries to add font awesome

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :landing_pages
 
+  resources :users
+
   get '/dash', to: 'dash#dash'
 
   root to: 'landing_pages#index'
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_221504) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "role"
+    t.integer "role", default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
