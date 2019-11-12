34be469064c20aadc0c7c39b14a4284fb5205811
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-11-12 09:11:11 -0500

    changes : to . on checkpoint card

--- app/javascript/components/checkpoints/checkpointcard.vue
+++ app/javascript/components/checkpoints/checkpointcard.vue
@@ -3,7 +3,7 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-8" v-on:click="changerShowRest">
-                    <h5 class="card-title">{{ checkpoint.order_number }}: {{ checkpoint.title }}</h5>
+                    <h5 class="card-title">{{ checkpoint.order_number }}. {{ checkpoint.title }}</h5>
                     <p v-if="showRest" class="card-text">
                         Description:<br />{{ checkpoint.description }}<br /><br />
                         Time to complete: {{ checkpoint.time_to_complete }}%
