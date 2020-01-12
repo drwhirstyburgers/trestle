2564fa3cc175f2bcb0a2ab2c4df0b125ee4109e5
David <dave.hirst50@gmail.com>
2020-01-11 20:25:16 -0500

    tryna fix

---
+++ app/javascript/assets/loadscripts.js
@@ -0,0 +1,9 @@
+function loadScripts(){
+    var tag = document.createElement("script");
+    tag.src = "https://code.getmdl.io/1.3.0/material.min.js";
+    document.getElementsByTagName("head")[0].appendChild(tag);
+}
+
+document.addEventListener('turbolinks:before-render', () => {
+    loadScripts();
+});
