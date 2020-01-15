ae217c3a8cea3c3f4684a648a42c1a800003ea7f
David <dave.hirst50@gmail.com>
2020-01-15 09:59:47 -0500

    adds some recursive functions to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
 module ApplicationHelper
 
+    #Two recusive methods, the first acts like .map the other does what .index does 
     def map_recur(arr, idx=0, acc=[], &findBy)
         return acc if idx == arr.count
         if findBy.call(arr[idx])
-          acc << arr[idx]
+            acc << arr[idx]
         end
-        find_by_condition_recur(arr, idx + 1, acc, &findBy)
+        map_recur(arr, idx + 1, acc, &findBy)
     end
 
-    def index_of(arr, toFind, count=0)
-        return arr[count] == toFind ? count : find_index_recur(arr, toFind, count += 1)
+    def index_of_recur(arr, toFind, idx=0)
+        return arr[count] == toFind ? count : index_of_recur(arr, toFind, idx += 1)
     end
+    
 end
