19c27925f1168e09f14271bf259c506ff271a035
David <dave.hirst50@gmail.com>
2020-01-18 22:00:11 -0500

    fixes courses index for admins

--- app/javascript/components/courses/courseindex.vue
+++ app/javascript/components/courses/courseindex.vue
@@ -3,13 +3,17 @@
     <div class="row">
         <div class="col-lg-1">
         </div>
-        <div class="col-lg-10">
-            <div v-if="courses.length > 0" v-for="course in courses" v-bind:course="course" v-bind:key="course.key" class="col-md-4 d-flex align-items-stretch">
-                <coursecard :ic="course" />
+        <div v-if="courses.length > 0" class="col-lg-10">
+            <div class="row">
+                <div v-for="course in courses" v-bind:course="course" v-bind:key="course.key" class="col-md-4 d-flex align-items-stretch">
+                    <coursecard :ic="course"/>
+                </div>
             </div>
-            <div v-else>
-                <h4>There are currently no courses! Come back soon!</h4>
-            </div>
+        </div>
+        <div v-else cclass="col-lg-10">
+        <div>
+            <h4>There are currently no courses! Come back soon!</h4>
+        </div>
         </div>
         <div class="col-lg-1">
         </div>
