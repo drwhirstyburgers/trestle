2d0776d779986514674d6820365e6ca30a71a64a
David <dave.hirst50@gmail.com>
2020-01-30 20:51:29 -0500

    adds users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
         @q = User.ransack(params[:q])
         @users = @q.result.includes(:courses).paginate(page: params[:page])
     end
+
+    def show
+        @user = User.find(params[:id])
+    end
 end
